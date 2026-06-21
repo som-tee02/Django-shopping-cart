81f0bef1139f16803ead766328f63b61eae801c5
Somto Obi <osomto669@gmail.com>
2026-06-21 08:22:10 +0100

    Emergency save: Database and Admin working

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return f"{self.name} (Stock: {self.stock})"
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from .models import Product
+from .cart import Cart
 
-# Create your views here.
+
+@require_POST
+def cart_add(request, product_id):
+    """Add a product to the cart or update its quantity."""
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+
+    # We will get the requested quantity from the HTML form (defaulting to 1)
+    # and check if the user is trying to override the exact quantity or just add to it.
+    quantity = int(request.POST.get('quantity', 1))
+    override = request.POST.get('override', False) == 'True'
+
+    # Send the data to the brain we built earlier
+    cart.add(product=product, quantity=quantity, override_quantity=override)
+
+    return redirect('cart:cart_detail')
+
+
+@require_POST
+def cart_remove(request, product_id):
+    """Remove a product entirely from the cart."""
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    """Display the contents of the cart."""
+    cart = Cart(request)
+
+    # We will pass the cart object to our HTML template soon
+    return render(request, 'cart/detail.html', {'cart': cart})
